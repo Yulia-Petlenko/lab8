e2c645c8475a21b5cf601223064c10feb77c2b2a
Юлия Петленко <juliaandreevnapetlenko@gmail.com>
2024-10-03 10:15:41 +0300

    Изменила функционал hello.pas

--- hello.pas
+++ hello.pas
@@ -1,3 +1,9 @@
 ﻿begin
-  print('Hello World');
+  var n:=ReadInteger();
+  case n of
+    4..10:print('Доброе утро, мир!');
+    11..16:print('Добрый день, мир!');
+    17..22:print('Добрый вечер, мир!');
+    23,0,1,2,3:print('Доброй ночи, мир!');
+  end;
 end.
